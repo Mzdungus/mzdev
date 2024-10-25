95aca6051efcf290d7cdbd00a591235594446652
Mahamat Zakaria Doungous <121716628+Mzdungus@users.noreply.github.com>
2024-10-26 00:00:16 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,20 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker enregistré avec succès :', registration);
+      })
+      .catch((error) => {
+        console.error('Échec de l\'enregistrement du Service Worker :', error);
+      });
+  });
+}
+
+
 (function() {
   "use strict";
 
